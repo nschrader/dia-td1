86f9b95b671bfc160991a805521f7fe7e6dbcc90
Nick Schrader <nick.schrader@mailbox.org>
2018-09-26 09:52:37 +0200

    server first try

--- server.js
+++ server.js
@@ -1,4 +1,35 @@
 const net = require('net');
 
+//TODO: Find better way...
+let dataResolver;
+const data = new Promise((resolve) => {
+  dataResolver = resolve;
+});
 
-// Implémenter votre serveur ici
+const server = net.Server((sock) => {
+  console.log('Connected to client');
+  sock.setEncoding('utf8');
+
+  sock.on('end', () => {
+    console.log('Client disconnected');
+  });
+
+  sock.on('data', dataResolver)
+});
+
+server.listen(8123, function() {
+  console.log('Server is listening');
+});
+
+server.on('error', (err) => {
+  console.error(err.toString());
+  server.end();
+});
+
+const handleCmd = (cmd) => {
+  if (/^PUT/.test(cmd)) {
+    console.log(put);
+  }
+}
+
+data.then(handleCmd);
