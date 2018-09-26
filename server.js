6ceaa0474feb826391ead6d6307c6f9a9279ec78
Nick Schrader <nick.schrader@mailbox.org>
2018-09-26 18:21:03 +0200

    server second try

--- server.js
+++ server.js
@@ -1,35 +1,50 @@
 const net = require('net');
 
-//TODO: Find better way...
-let dataResolver;
-const data = new Promise((resolve) => {
-  dataResolver = resolve;
-});
+const socket = new Promise((resolve, reject) => {
+  const server = net.Server((sock) => {
+    console.log('Connected to client');
+    sock.setEncoding('utf8');
 
-const server = net.Server((sock) => {
-  console.log('Connected to client');
-  sock.setEncoding('utf8');
+    sock.on('end', () => {
+      console.log('Client disconnected');
+      reject();
+    });
 
-  sock.on('end', () => {
-    console.log('Client disconnected');
+    resolve(sock);
   });
 
-  sock.on('data', dataResolver)
+  server.listen(8123, () => console.log('Server is listening'));
+
+  server.on('error', (err) => {
+    console.error(err.toString());
+    server.end();
+    reject(err);
+  });
 });
 
-server.listen(8123, function() {
-  console.log('Server is listening');
+const data = new Promise((resolve) => {
+  socket.then((sock) => sock.on('data', resolve));
 });
 
-server.on('error', (err) => {
-  console.error(err.toString());
-  server.end();
+data.then((cmd) => {
+  if (/^PUT/.test(cmd)) {
+    console.log('put');
+    socket.then((sock) => sock.write('ok\n'));
+  } else if (/^KEYS/.test(cmd)) {
+    console.log('keys');
+    socket.then((sock) => sock.write('deine mudda\n'));
+  } else {
+    console.log('shit');
+  }
 });
 
-const handleCmd = (cmd) => {
+
+
+
+/*const handleCmd = (cmd) => {
   if (/^PUT/.test(cmd)) {
     console.log(put);
   }
 }
 
-data.then(handleCmd);
+data.then(handleCmd);*/
